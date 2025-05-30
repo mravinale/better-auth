ba0790682e022ac3d2cd68a4a955215a2f7c00f8
Mariano Ravinale <mravinale@gmail.com>
2025-05-30 15:49:56 -0300

    minor test changes

--- IdP/test/api.e2e.test.js
+++ IdP/test/api.e2e.test.js
@@ -3,21 +3,27 @@ import request from 'supertest';
 import express from 'express';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from '../src/infrastructure/auth.js';
-import apiRoutes from '../src/infrastructure/routes.js';
 
 let server;
 const app = express();
 app.all('/api/auth/*', toNodeHandler(auth));
 app.use(express.json());
-app.use('/api', apiRoutes);
 
 beforeAll(async () => {
   // Assumes the PostgreSQL schema is already migrated and clean
   server = app.listen(0);
 });
 
-afterAll((done) => {
-  server.close(done);
+afterAll(async () => { 
+  await new Promise((resolve, reject) => {
+    server.close((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  }); 
+  if (auth.database && typeof auth.database.end === 'function') {
+    await auth.database.end(); 
+  } 
 });
 
 describe('Better Auth API E2E', () => {
@@ -27,7 +33,6 @@ describe('Better Auth API E2E', () => {
   const name = 'Test User';
 
   it('should sign up a new user', async () => {
-    console.log('[E2E] About to send sign-up request');
     const res = await request(server)
       .post('/api/auth/sign-up/email')
       .send({ email, password, name });
